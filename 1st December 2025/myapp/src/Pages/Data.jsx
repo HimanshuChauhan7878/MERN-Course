62d9d427417769d32cfa40ebd8fc4382a649b90a
HimanshuChauhan7878 <himanshu34484@gmail.com>
2025-12-01 19:47:10 +0530

    Routing

---
+++ 1st December 2025/myapp/src/Pages/Data.jsx
@@ -0,0 +1,72 @@
+import svg1 from "../Pages/images/svg-1.svg";
+import profileImg from "../Pages/images/profile.jpg";
+import svg2 from "../Pages/images/svg-2.svg";
+import svg3 from "../Pages/images/svg-3.svg";
+
+export const homeObjOne1 = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  topLine: "Marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  start: "",
+  img: svg1,
+  alt: "Image",
+};
+
+export const homeObjOne2 = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  buttonLabel: "Get Started",
+  topLine: "Marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  start: "true",
+  img: profileImg,
+  alt: "Image",
+};
+
+export const homeObjOne3 = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  topLine: "Marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  start: "",
+  img: svg2,
+  alt: "Image",
+};
+
+export const homeObjOne4 = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  buttonLabel: "Get Started",
+  topLine: "Marketing Agency",
+  headline: "Lead Generation Specialist for Online Business",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  start: "true",
+  img: svg3,
+  alt: "Image",
+};
